8293eccd0500af608cc49de6d4e70bb1b0d9cef1
NOKUBI Takatsugu <knok@daionet.gr.jp>
2014-10-08 16:11:40 +0900

    Fix string treatment

--- mikutter-perl.rb
+++ mikutter-perl.rb
@@ -18,20 +18,21 @@ Plugin.create(:mikutter_perl) do
 
   Perl::init_perl
 
-#  notice "mikutter-perl"
-
-  filter_before_postbox_post do |m|
-    if m.include?("#perl")
-      begin
-        str = m.gsub("#perl", "")
-        ret = Perl::eval_perl(str)
-        #      activity :system, str
-        print ret, "\n"
-      rescue => e
-        print "exception\n"
-        pp e
+  filter_posted do |service, msgs|
+    msgs.each do |m|
+      #pp m
+      if m.to_s.include?("#perl")
+        begin
+          str = m.to_s.gsub("#perl", "")
+          ret = Perl::eval_perl(str)
+          activity :system, ret.to_s
+        rescue => e
+          print "exception\n"
+          pp e
+        end
       end
     end
+    [msgs]
   end
 
 end
